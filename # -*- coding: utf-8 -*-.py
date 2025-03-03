82bff13554509084fdceb0f06acdb818bc70e79a
Oscar Lijeron <olijeron001@ikasle.ehu.eus>
2025-03-03 13:49:06 +0100

    Añadido primera version

--- # -*- coding: utf-8 -*-.py
+++ # -*- coding: utf-8 -*-.py
@@ -1,104 +1,67 @@
-# -*- coding: utf-8 -*-
-"""
-Autor: Xabier Gabiña Barañano
-Script para la implementación del algoritmo kNN
-Recoge los datos de un fichero csv y los clasifica en función de los k vecinos más cercanos
-"""
+# This is a sample Python script.
 
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import getopt
 import sys
-import sklearn as sk
 import numpy as np
 import pandas as pd
+import sklearn as sk
+import imblearn
 import pickle
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 
-def load_data(file):
-    """
-    Función para cargar los datos de un fichero csv
-    :param file: Fichero csv
-    :return: Datos del fichero
-    """
-    data = pd.read_csv(file)
-    return data
 
-def calculate_fscore(y_test, y_pred):
-    """
-    Función para calcular el F-score
-    :param y_test: Valores reales
-    :param y_pred: Valores predichos
-    :return: F-score (micro), F-score (macro)
-    """
-    from sklearn.metrics import f1_score
-    fscore_micro = f1_score(y_test, y_pred, average='micro')
-    fscore_macro = f1_score(y_test, y_pred, average='macro')
-    return fscore_micro, fscore_macro
+model=""
+p="./"
 
-def calculate_confusion_matrix(y_test, y_pred):
-    """
-    Función para calcular la matriz de confusión
-    :param y_test: Valores reales
-    :param y_pred: Valores predichos
-    :return: Matriz de confusión
-    """
-    from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(y_test, y_pred)
-    return cm
-
-def kNN(data, k, weights, p):
-    """
-    Función para implementar el algoritmo kNN
-    
-    :param data: Datos a clasificar
-    :type data: pandas.DataFrame
-    :param k: Número de vecinos más cercanos
-    :type k: int
-    :param weights: Pesos utilizados en la predicción ('uniform' o 'distance')
-    :type weights: str
-    :param p: Parámetro para la distancia métrica (1 para Manhattan, 2 para Euclídea)
-    :type p: int
-    :return: Clasificación de los datos
-    :rtype: tuple
-    """
-    # Seleccionamos las características y la clase
-    X = data.iloc[:, :-1].values # Todas las columnas menos la última
-    y = data.iloc[:, -1].values # Última columna
-    
-    # Dividimos los datos en entrenamiento y test
-    from sklearn.model_selection import train_test_split
-    np.random.seed(42)  # Set a random seed for reproducibility
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-    
-    # Escalamos los datos
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    
-    # Entrenamos el modelo
-    from sklearn.neighbors import KNeighborsClassifier
-    classifier = KNeighborsClassifier(n_neighbors = k, weights = weights, p = p)
-    classifier.fit(X_train, y_train)
-    
-    # Predecimos los resultados
-    y_pred = classifier.predict(X_test)
-    
-    return y_test, y_pred
-
-if __name__ == "__main__":
-    # Comprobamos que se han introducido los parámetros correctos
-    if len(sys.argv) < 3:
-        print("Error en los parámetros de entrada")
-        print("Uso: kNN.py <fichero*> <k*> <weights> <p>")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('ARGV   :',sys.argv[1:])
+    try:
+        options,remainder = getopt.getopt(sys.argv[1:],'p:m:f:h',['path=','model=','testFile=','h'])
+    except getopt.GetoptError as err:
+        print('ERROR:',err)
         sys.exit(1)
-    
-    # Cargamos los datos
-    data = load_data(sys.argv[1])
-    
-    # Implementamos el algoritmo kNN
-    y_test, y_pred = kNN(data, int(sys.argv[2]), sys.argv[3] if len(sys.argv) > 3 else 'uniform', int(sys.argv[4]) if len(sys.argv) > 4 else 2)
-    
-    # Mostramos la matriz de confusión
-    print("\nMatriz de confusión:")
-    print(calculate_confusion_matrix(y_test, y_pred))
+    print('OPTIONS   :',options)
 
-    # Mostramos el F-score
-    print("\
+    for opt,arg in options:
+        if opt in ('-p','--path'):
+            p = arg
+        elif opt in ('-f', '--file'):
+            f = arg
+        elif opt in ('-m', '--model'):
+            m = arg
+        elif opt in ('-h','--help'):
+            print(' -p modelAndTestFilePath \n -m modelFileName -f testFileName\n ')
+            exit(1)
+
+    
+    if p == './':
+        model=p+str(m)
+        iFile = p+ str(f)
+    else:
+        model=p+"/"+str(m)
+        iFile = p+"/" + str(f)
+        
+
+    #Abrir el fichero .csv con las instancias a predecir y que no contienen la clase y cargarlo en un dataframe de pandas para hacer la prediccion
+    y_test=pd.DataFrame()
+    testX = pd.read_csv(iFile)
+
+    print(testX.head(5))
+    clf = pickle.load(open(model, 'rb'))
+    predictions = clf.predict(testX)
+    probas = clf.predict_proba(testX)
+    y_test['preds'] = predictions
+    predictions = pd.Series(data=predictions, index=testX.index, name='predicted_value')
+    results_test = testX.join(predictions, how='left')
+    
+    print(results_test) # Imprimir el resultado de la prediccion
+    
